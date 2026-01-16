911bea1a337a7cf063632647d8a1e88d7752cc75
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2026-01-16 17:18:38 +0530

    print the increasing number in pattern

--- DSA/StarPattern.js
+++ DSA/StarPattern.js
@@ -11,3 +11,31 @@ for (let i = 0; i < 4; i++) {
 // *****
 // *****
 // *****
+
+for (let i = 0; i < 4; i++) {
+  let row = "";
+  for (let j = 0; j < i + 1; j++) {
+    row = row + "*";
+  }
+  console.log(row);
+}
+
+// output:
+// *
+// **
+// ***
+// ****
+
+for (let i = 0; i <= 5; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row = row + j;
+  }
+  console.log(row);
+}
+// output:
+// 1
+// 12
+// 123
+// 1234
+// 12345
