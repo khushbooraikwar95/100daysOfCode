c05e28af7982c3b79983846d9f5a6dc294772a6c
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2026-01-16 17:37:36 +0530

    reverse pattern number descreasing order

--- DSA/StarPattern.js
+++ DSA/StarPattern.js
@@ -41,11 +41,11 @@ for (let i = 0; i <= 5; i++) {
 // 12345
 
 let n = 5;
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n; i--) {
   let row = "";
-  for (let j = 0; j <= i; j++) {
-    row = row + (i + 1);
-  }
+  //   for (let j = 0; j <= i; j++) {
+  //     row = row + (i + 1);
+  //   }
   console.log(row);
 }
 
@@ -55,3 +55,19 @@ for (let i = 0; i < n; i++) {
 // 333
 // 4444
 // 55555
+
+let n = 5;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n - i; j++) {
+    row = row + (j + 1);
+  }
+  console.log(row);
+}
+
+//output
+// 12345
+// 1234
+// 123
+// 12
+// 1
