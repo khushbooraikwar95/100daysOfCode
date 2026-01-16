dc3950d31554f3a3798c9e23dbe168ab4740c831
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2026-01-16 17:29:40 +0530

    number increasing pattern

--- DSA/StarPattern.js
+++ DSA/StarPattern.js
@@ -39,3 +39,19 @@ for (let i = 0; i <= 5; i++) {
 // 123
 // 1234
 // 12345
+
+let n = 5;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j <= i; j++) {
+    row = row + (i + 1);
+  }
+  console.log(row);
+}
+
+// output:
+// 1
+// 22
+// 333
+// 4444
+// 55555
