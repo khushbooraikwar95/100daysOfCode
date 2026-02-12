29834c7ca46d04ed9aeb0004bf633b6739b3cc03
Khushboo Raikwar <35090657+khushbooraikwar95@users.noreply.github.com>
2026-02-12 20:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (96.42%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,18 +3,9 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let hash = {}
-
-    for(let i=0; i<nums.length; i++){
-        if(!hash[nums[i]]){
-            hash[nums[i]] = 1
-        }else {
-            hash[nums[i]] ++;
-        }
-    }
-    for(let i=0; i<nums.length; i++){
-        if(hash[nums[i]] == 1){
-            return nums[i]
-        }
-    }
+   let xor = 0;
+   for( let i=0; i<nums.length; i++){
+    xor = xor ^ nums[i]
+   }
+   return xor
 };
